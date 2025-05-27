46dd6ae2f3bd35a3891171a6309a9bc32421e00d
TilahuneMekonnen <tilahunemihreta@gmail.com>
2025-05-26 20:05:18 -0400

    carousel css remove the gap between images and header part

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Carousel as ResponsiveCarousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { images } from "./img/Data";
+import { img } from "./img/data"; // ✅ lowercase
 import "./Carousel.css";
 
 const Carousel = () => {
@@ -14,9 +14,9 @@ const Carousel = () => {
         showStatus={false}
         interval={3000}
       >
-        {images.map((img, index) => (
+        {img.map((image, index) => (
           <div className="carousel-slide" key={index}>
-            <img src={img} alt={`slide-${index}`} />
+            <img src={image} alt={`slide-${index}`} />
           </div>
         ))}
       </ResponsiveCarousel>
